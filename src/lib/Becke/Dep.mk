40a8b970eb6775bbaebc81ca7b917c9498cd3a01
allouchear <allouchear@gmail.com>
2024-07-25 17:40:36 +0200

    update README.md

--- src/lib/Becke/Dep.mk
+++ src/lib/Becke/Dep.mk
@@ -1,71 +1,72 @@
 GridPoints6.o: GridPoints6.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints14.o: GridPoints14.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints26.o: GridPoints26.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints38.o: GridPoints38.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints50.o: GridPoints50.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints74.o: GridPoints74.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints86.o: GridPoints86.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints110.o: GridPoints110.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints146.o: GridPoints146.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints170.o: GridPoints170.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints194.o: GridPoints194.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints230.o: GridPoints230.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints266.o: GridPoints266.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints302.o: GridPoints302.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints350.o: GridPoints350.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints434.o: GridPoints434.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints590.o: GridPoints590.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints770.o: GridPoints770.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints974.o: GridPoints974.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints1202.o: GridPoints1202.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints1454.o: GridPoints1454.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints1730.o: GridPoints1730.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints2030.o: GridPoints2030.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints2354.o: GridPoints2354.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints5810.o: GridPoints5810.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
 GridPoints.o: GridPoints.cpp \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
-Becke.o: Becke.cpp /home/dbuffat/CdftT/src/lib/Cube/Grid.h \
- /home/dbuffat/CdftT/src/lib/Common/Structure.h \
- /home/dbuffat/CdftT/src/lib/Common/Atom.h \
- /home/dbuffat/CdftT/src/lib/Common/PeriodicTable.h \
- /home/dbuffat/CdftT/src/lib/Common/Element.h \
- /home/dbuffat/CdftT/src/lib/Common/Element.h \
- /home/dbuffat/CdftT/src/lib/Utils/WFX.h \
- /home/dbuffat/CdftT/src/lib/Utils/Utils.h \
- /home/dbuffat/CdftT/src/lib/Utils/FCHK.h \
- /home/dbuffat/CdftT/src/lib/Utils/MOLDENGAB.h \
- /home/dbuffat/CdftT/src/lib/Utils/LOG.h \
- /home/dbuffat/CdftT/src/lib/Cube/Domain.h \
- /home/dbuffat/CdftT/src/lib/Becke/Becke.h \
- /home/dbuffat/CdftT/src/lib/Orbitals/Orbitals.h \
- /home/dbuffat/CdftT/src/lib/Basis/CGTF.h \
- /home/dbuffat/CdftT/src/lib/Basis/GTF.h \
- /home/dbuffat/CdftT/src/lib/Common/Descriptors.h \
- /home/dbuffat/CdftT/src/lib/Cube/GridCP.h \
- /home/dbuffat/CdftT/src/lib/Becke/GridPoints.h
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
+Becke.o: Becke.cpp \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Cube/Grid.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Common/Structure.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Common/Atom.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Common/PeriodicTable.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Common/Element.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Common/Element.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Utils/WFX.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Utils/Utils.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Utils/FCHK.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Utils/MOLDENGAB.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Utils/LOG.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Cube/Domain.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/Becke.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Orbitals/Orbitals.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Basis/CGTF.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Basis/GTF.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Common/Descriptors.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Cube/GridCP.h \
+ /home/allouche/MySoftwares/CdftT/CdftT/src/lib/Becke/GridPoints.h
