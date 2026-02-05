20c543a26818dd575bc380d637fcbf319e581630
BissuelD <dylan.bissuel@gmail.com>
2026-02-05 14:19:04 +0100

    cleaner makefiles

--- src/lib/Basis/Dep.mk
+++
@@ -1,7 +0,0 @@
-GTF.o: GTF.cpp /usr/home/lgardre-local/Work/CdftT/src/lib/Basis/GTF.h \
- /usr/home/lgardre-local/Work/CdftT/src/lib/Utils/Utils.h \
- /usr/home/lgardre-local/Work/CdftT/src/lib/Utils/Utils_fileParsing.tpp
-CGTF.o: CGTF.cpp /usr/home/lgardre-local/Work/CdftT/src/lib/Basis/CGTF.h \
- /usr/home/lgardre-local/Work/CdftT/src/lib/Basis/GTF.h \
- /usr/home/lgardre-local/Work/CdftT/src/lib/Utils/Utils.h \
- /usr/home/lgardre-local/Work/CdftT/src/lib/Utils/Utils_fileParsing.tpp
